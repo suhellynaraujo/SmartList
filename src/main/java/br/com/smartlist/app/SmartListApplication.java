205a4216fe909f994ad4723f00a4429b7d909dff
Suhellyn Araujo <github.interface@gmail.com>
2025-03-14 20:30:48 -0300

    iniciando repositorio

---
+++ src/main/java/br/com/smartlist/app/SmartListApplication.java
@@ -0,0 +1,13 @@
+package br.com.smartlist.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartListApplication.class, args);
+	}
+
+}
